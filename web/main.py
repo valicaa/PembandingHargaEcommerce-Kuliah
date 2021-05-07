b535ee1fdf8b10ec87834177bc636d8784a7dd3c
root <root@DESKTOP-5S4KTQK.localdomain>
2021-05-07 23:52:41 +0700

    Add getimage function in scraper and added requests package in requirements

--- web/main.py
+++ web/main.py
@@ -1,11 +1,12 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, render_template, send_file
 
 from controller import database_connector
+from scraper import shopee
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
@@ -17,6 +18,11 @@ def database_connect():
     query = 'INSERT INTO ecommerce (id_ecommerce,nama_ecommerce) VALUES ("2", "Tokopedia")'
     db = database_connector.Database()
     return db.run(query)
+
+@app.route('/api/getimage/<string:imagestring>', methods = ['GET'])
+def getimage(imagestring):
+    success = shopee.ShopeeScraper().getimage(imagestring)
+    return send_file("./static/"+imagestring+".png", mimetype='image/gif')
 
 if __name__ == '__main__':
     app.run()
